c4df3499cd7871609203a03907130345d502aac7
Roger Walker <roger.john.walker@gmail.com>
2024-12-02 20:21:30 -0500

    day 2 parts 1 and 2

---
+++ lib/day2.ex
@@ -0,0 +1,64 @@
+defmodule Day2 do
+
+  def read_input(name) do
+    File.read!("./input/#{name}")
+    |> String.split("\r\n", trim: true)
+    |> Enum.map(fn report ->
+              String.split(report, " ", trim: true)
+              |> Enum.map(fn l ->
+                {level, _} = Integer.parse(l)
+                level
+              end)
+            end)
+  end
+
+  def get_safe_reports(name) do
+    read_input(name)
+    |> Enum.count(&safe?/1)
+  end
+
+  def get_dampened_safe_reports(name) do
+    read_input(name)
+    |> Enum.count(&prob_dampener/1)
+  end
+
+  def safe?(levels) do
+    asc_or_desc?(levels) && do_check_bounds(levels)
+  end
+
+  def asc_or_desc?(levels) do
+    sorted = Enum.sort(levels)
+    reversed = Enum.reverse(sorted)
+
+    sorted == levels || reversed == levels
+  end
+
+  def do_check_bounds([_]), do: true
+
+  def do_check_bounds([a, b | rest]) do
+    case with_in_bounds?(a,b) do
+      true -> do_check_bounds([b | rest])
+      false -> false
+    end
+  end
+
+  def with_in_bounds?(a,b) when a >= b + 1 and a <= b + 3, do: true
+  def with_in_bounds?(b,a) when a >= b + 1 and a <= b + 3, do: true
+  def with_in_bounds?(_,_), do: false
+
+  def prob_dampener(levels) do
+    case safe?(levels) do
+      true -> true
+      false -> do_dampening([], levels)
+    end
+  end
+
+  def do_dampening(start, []), do: safe?(start)
+  def do_dampening(start, [h | rest]) do
+    case safe?(start ++ rest) do
+      true -> true
+      false -> do_dampening( start ++ [h], rest)
+    end
+  end
+
+end
--- test/day_1_test.exs
+++ test/day_1_test.exs
@@ -1,16 +1,19 @@
 defmodule Day1Test do
   use ExUnit.Case
 
+  @tag :pending
   test "read_input" do
     assert Day1.read_input("day1test") == {[3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]}
   end
 
+  @tag :pending
   test "find_distance" do
     a = [3, 4, 2, 1, 3, 3]
     b = [4, 3, 5, 3, 9, 3]
     assert Day1.find_distance({a, b}) == 11
   end
 
+  @tag :pending
   test "find_day1_part1" do
     distance =
       Day1.read_input("day1input")
@@ -19,12 +22,14 @@ defmodule Day1Test do
     assert distance == 1506483
   end
 
+  @tag :pending
   test "find_similarity_score" do
     a = [3, 4, 2, 1, 3, 3]
     b = [4, 3, 5, 3, 9, 3]
     assert Day1.find_similarity_score({a, b}) == 31
   end
 
+  @tag :pending
   test "find_day1_part2" do
     similarity =
       Day1.read_input("day1input")
